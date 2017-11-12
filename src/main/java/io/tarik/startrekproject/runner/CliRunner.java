c22db83c51659b9ef7ec51701285dbfe7bee92b5
Tarik Demirci <git@mail.tarik.io>
2017-11-12 20:35:22 +0300

    Wire klingon translation service to command line runner.

--- src/main/java/io/tarik/startrekproject/runner/CliRunner.java
+++ src/main/java/io/tarik/startrekproject/runner/CliRunner.java
@@ -1,6 +1,8 @@
 package io.tarik.startrekproject.runner;
 
+import io.tarik.startrekproject.klingon.service.KlingonService;
 import io.tarik.startrekproject.stapi.service.StapiService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -9,17 +11,22 @@ import org.springframework.stereotype.Component;
 public class CliRunner implements CommandLineRunner{
 
     private final StapiService stapiService;
+    private final KlingonService klingonService;
 
     @Autowired
-    public CliRunner(StapiService stapiService) {
+    public CliRunner(StapiService stapiService, KlingonService klingonService) {
         this.stapiService = stapiService;
+        this.klingonService = klingonService;
     }
 
     @Override
     public void run(String... args) throws Exception {
         if (args.length > 0) {
+            String name = args[0];
+            klingonService.translateToKlingon(name)
+                    .ifPresent(klingonChars -> System.out.println(StringUtils.join(klingonChars, " ")));
             stapiService.getSpeciesOfName(args[0])
-                    .ifPresent(species -> System.out.println("Hello World: " + species));
+                    .ifPresent(species -> System.out.println(StringUtils.join(species, " ")));
         } else {
             System.out.println("Please specify <name>");
         }
