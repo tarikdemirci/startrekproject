f94773b1216258f18bda14155f79cf9d9f8e7084
Tarik Demirci <git@mail.tarik.io>
2017-11-12 18:58:12 +0300

    Implement PlqadMapClient.getEnglishToPlqadMapping()

--- src/main/java/io/tarik/startrekproject/klingon/external/PlqadMapClient.java
+++ src/main/java/io/tarik/startrekproject/klingon/external/PlqadMapClient.java
@@ -1,13 +1,47 @@
 package io.tarik.startrekproject.klingon.external;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+
 
 @Component
 public class PlqadMapClient {
-    public Map<String, String> getEnglishToPlqadMapping() {
-        return new HashMap<>();
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final ObjectMapper objectMapper;
+
+    @Value("${klingon.englishToPlqadMapping.file.name}")
+    private String englishToPlqadMappingFile;
+
+    @Autowired
+    public PlqadMapClient(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    public Optional<Map<String, String>> getEnglishToPlqadMapping() {
+        try {
+            Resource resource = new ClassPathResource(new File("klingon", englishToPlqadMappingFile).getPath());
+            InputStream resourceInputStream = resource.getInputStream();
+            TypeReference<HashMap<String, String>> typeRef
+                    = new TypeReference<HashMap<String, String>>() {};
+
+            return Optional.of(objectMapper.readValue(resourceInputStream, typeRef));
+        } catch (IOException e) {
+            logger.error("Unable to get mapping from file: " + englishToPlqadMappingFile, e);
+            return Optional.empty();
+        }
     }
 }
--- src/test/java/io/tarik/startrekproject/klingon/external/PlqadMapClientTest.java
+++ src/test/java/io/tarik/startrekproject/klingon/external/PlqadMapClientTest.java
@@ -2,25 +2,29 @@ package io.tarik.startrekproject.klingon.external;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Map;
+import java.util.Optional;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PlqadMapClientTest {
-    @InjectMocks
+    @Autowired
     private PlqadMapClient plqadMapClient;
 
     @Test
     public void getEnglishToPlqadMapping() throws Exception {
-        Map<String, String> mapping = plqadMapClient.getEnglishToPlqadMapping();
+        Optional<Map<String, String>> optionalMapping = plqadMapClient.getEnglishToPlqadMapping();
+        assertTrue(optionalMapping.isPresent());
+
+        Map<String, String> mapping = optionalMapping.get();
         assertEquals("0xF8E5", mapping.get("u"));
-        assertEquals("0xF8D6", mapping.get("h"));
+        assertEquals("0xF8D6", mapping.get("H"));
         assertEquals("0xF8E1", mapping.get("r"));
         assertEquals("0xF8D0", mapping.get("a"));
     }
